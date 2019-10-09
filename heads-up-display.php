a647c0cc989416c0a5936e054c31228cbfc0ad33
padillac <shinimaninima@gmail.com>
2019-10-08 22:31:27 -0500

    first commit to heads-up-display plugingit add .

---
+++ heads-up-display.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Heads Up Display
+Description: This plugin creates a box dislaying the number of posts on the site, in a configurable location
+Author: Chris Padilla
+Version: 1.0
+*/
