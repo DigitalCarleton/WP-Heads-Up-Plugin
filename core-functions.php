8550ac863864a21fb25b93c6d23ebccaca163589
Alvin Bierley <alvinbierley@eduroam-170-191.dyn.carleton.edu>
2020-11-05 15:25:17 -0600

    adding display functionality

--- core-functions.php
+++ core-functions.php
@@ -21,4 +21,42 @@ function headsup_function($arg) {
   echo "{$styling[$styleInfo][0]}Most Recent Post: " . date( 'jS F, Y', strtotime( $recentPosts[0]['post_date'] ) ) . "{$styling[$styleInfo][1]}<br>";
 }
 
-add_action('rightnow_end', 'headsup_function');
+/**
+ * Add a new dashboard widget.
+ */
+function wpdocs_add_dashboard_widgets() {
+  wp_add_dashboard_widget( 'dashboard_widget', 'Heads Up Display Extension', 'headsup_function' );
+}
+
+/**
+ * Formats display of information on the main page
+ */
+function main_page_display() {
+  ?>
+  <div style="margin-left:2.75%; font-size:15px; margin-top:1%; margin-bottom:1%;">
+    <?php headsup_function() ?>
+  </div>
+<?php
+}
+
+/**
+ * Chooses where to display plugin content
+ */
+function display_content() {
+  $locationInfo = get_option('headsup_options')['location'];
+  switch ($locationInfo) {
+    case "At a glance":
+      add_action( 'rightnow_end', 'headsup_function' );
+      break;
+    case "Heads Up Widget":
+      add_action( 'wp_dashboard_setup', 'wpdocs_add_dashboard_widgets' );
+      break;
+    case "Main page":
+      add_action( 'wp_head', 'main_page_display' );
+      break;
+    default:
+      add_action( 'rightnow_end', 'headsup_function' );
+  }
+}
+
+display_content();
