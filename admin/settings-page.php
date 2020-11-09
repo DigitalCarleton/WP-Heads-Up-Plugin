5f8f8f608249b377b9153c303e14985e0de6c40c
Alvin Bierley <alvinbierley@eduroam-170-191.dyn.carleton.edu>
2020-11-08 21:51:44 -0600

    new settings page

--- admin/settings-page.php
+++ admin/settings-page.php
@@ -6,71 +6,55 @@ if (!defined('ABSPATH')) {
   exit;
 }
 
-// display the plugin settings page
-function headsup_display_settings_page() {
+function headsup_add_settings_page() {
+	add_options_page( 'Headsup plugin page', 'Headsup Plugin Menu', 'manage_options', 'headsup_plugin', 'headsup_display_settings_page' );
+}    
+add_action( 'admin_menu', 'headsup_add_settings_page' );
 
+function headsup_display_settings_page() {
 	// check if user is allowed access
 	if ( ! current_user_can( 'manage_options' ) ) return;
-
 	?>
-
-	<div class="wrap">
-		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+	<!-- Settings page display -->
+	<h1>Headsup Plugin Settings</h1>
 		<form action="options.php" method="post">
-
-			<?php
-
-			// output security fields
+			<?php 
 			settings_fields( 'headsup_options' );
-
-			// output setting sections
-			do_settings_sections( 'headsup' );
-
-			// submit button
-			submit_button();
-
+			do_settings_sections( 'headsup_plugin' ); 
 			?>
-
+			<input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
 		</form>
-	</div>
-
-	<?php
-
+		<?php
 }
 
-
-
+function headsup_plugin_section_text() {
+	echo '<p>Choose the font style and display location</p>';
+}
 
 // register plugin settings
 function headsup_register_settings() {
+	register_setting( 'headsup_options', 'headsup_options', 'headsup_callback_validate_options' );
 
+	add_settings_section( 'api_settings', 'Display Options', 'headsup_plugin_section_text', 'headsup_plugin' );
 
-	register_setting(
-		'headsup_options',
-		'headsup_options',
-		'headsup_callback_validate_options'
+	add_settings_field(
+		'headsup_callback_font',
+		'Font Style',
+		'headsup_callback_font',
+		'headsup_plugin',
+		'api_settings',
+		[ 'id' => 'font_style', 'label' => 'Custom display location' ]
 	);
 
-  add_settings_section(
-    'headsup_section_1',
-    'Choose the font style for Heads Up',
-    'headsup_callback_section_1',
-    'headsup'
-  );
-
-
-
-  add_settings_field(
-    'font_style',
-    'Font Style',
-    'headsup_callback_field_radio',
-    'headsup',
-    'headsup_section_1',
-    [ 'id' => 'font_style', 'label' => 'Custom display location' ]
-  );
-
+	add_settings_field(
+		'headsup_callback_location',
+		'Location',
+		'headsup_callback_location',
+		'headsup_plugin',
+		'api_settings',
+		[ 'id' => 'location', 'label' => 'Custom display location' ]
+	);
 }
-
 add_action( 'admin_init', 'headsup_register_settings' );
 
 
